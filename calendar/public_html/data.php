b7fb2c9ebfac9886a880ee16c1565e88d2a0a626
carrey120 <62525556+carrey120@users.noreply.github.com>
2020-06-05 20:29:02 +0800

    Add files via upload

---
+++ calendar/public_html/data.php
@@ -0,0 +1,34 @@
+<?php 
+
+$dates = [];
+for ($i=1; $i<=31;$i++){
+    $dates[] = $i;
+}
+
+$dates [] = null;
+$dates [] = null;
+$dates [] = null;
+$dates [] = null;
+// include('../db.php');
+// echo 'abc';
+
+// 用PDO連接到MYsql
+// try {
+//     $pdo = new PDO("mysql:host=$db[host];dbname=$db[dbname];port=$db[port];charset=$db[charset]",
+//     $db['username'], $db['password']);
+// } catch(PDOException $e){
+//     echo "Database connection failed.";
+//     exit;
+// }
+
+// ASC = 递增排序
+// $sql = 'SELECT * FROM `todos` ORDER BY `order` ASC';
+// $statement = $pdo->prepare($sql);
+// $statement->execute();
+// $todos = $statement->fetchAll(PDO::FETCH_ASSOC);
+// ?>
+
+
+<!-- <script>
+var todos = <?= json_encode($todos,JSON_NUMERIC_CHECK) ?>
+</script> -->
---
+++ calendar/public_html/index.php
@@ -0,0 +1,36 @@
+<?php include('header.php') ?>
+<?php include('data.php') ?>
+
+<div id="calendar">
+    <div id="header">
+        <?= date('Y')?>/<?= date('m')?>
+    </div>
+
+    <div id="days" class="clearfix">
+        <div class="day">SUN</div>
+        <div class="day">MON</div>
+        <div class="day">TUE</div>
+        <div class="day">WED</div>
+        <div class="day">THU</div>
+        <div class="day">FRI</div>
+        <div class="day">SAT</div>
+    </div>
+ 
+    <div id="dates" class="clearfix">
+        <?php foreach ($dates as $key => $date): ?>
+            <div class="date-block">
+            
+                <div class="date"><?= $date ?></div>
+                <div class="events">
+                    
+                </div>
+            </div>
+        <?php endforeach ?>
+    </div>
+</div>
+
+
+
+
+
+<?php include('footer.php') ?>
